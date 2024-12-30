123dd78699f68369cbd2cc8f68449e9dcc95bc3a
Declan <declan@nomlas.design>
2024-12-30 16:51:46 +1100

    Back at it

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Scene from '@/app/components/Scene';
-import Grid from '@/app/components/Grid';
 
 export default function Home() {
   return (
     <div>
-      {/* <Grid /> */}
       <Scene />
     </div>
   );
